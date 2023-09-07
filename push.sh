5a8d7678db2973f723f5aa125b705112d97a31ad
3053063553 <3053063553@qq.com>
2023-09-07 11:54:47 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.name '3053063553'
 git config --global user.email '3053063553@qq.com'
 git config --global http.sslverify false
 git add .
-git commit -m $1
+git commit -m 自动推送
 git push
